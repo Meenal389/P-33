fda007fa4c1edb187b29e3828d80c045e088c5ba
Meenal389 <74965143+Meenal389@users.noreply.github.com>
2021-01-04 01:00:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,119 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Events = Matter.Events;
+const Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions=[];
+
+var divisionHeight=300;
+var score =0;
+var particle;
+var turn=0;
+var gameState="Start";
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+  g2=new Ground(5,400,10,800)
+  g3=new Ground(795,400,10,800)
+
+  
+  //object=Bodies.rectangle(x,y,width,height)
+  
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+}
+ 
+
+
+function draw() {
+  background("black");
+  ground.display()
+  g2.display()
+  g3.display()
+
+  textFont("Times New Roman")
+  textSize(30)
+ text("Score : "+score,20,30);
+ text("200",20,520)
+ text("500",100,520)
+ text("100",180,520)
+ text("700",260,520)
+ text("400",340,520)
+ text("100",420,520)
+ text("500",500,520)
+ text("300",580,520)
+ text("900",660,520)
+ text("100",740,520)
+  Engine.update(engine);
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   /*if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }*/
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+
+   push()
+   stroke("yellow")
+   strokeWeight(2)
+   line(10,480,790,480);
+   pop()
+
+   if(turn===5){
+     gameState="end";
+   }
+}
+
+function mousePressed(){
+   if(gameState!="end"){
+     turn+=1
+     particle=new Particle(mouseX,10,8)
+   }
+   if(particle!=null){
+     particle.display()
+   }
+}
